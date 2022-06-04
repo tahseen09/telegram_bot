8bbd9e585a70437af8cbe08f1c6d2a3895024cca
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2022-06-05 01:08:01 +0530

    correct path for fixtures file

--- hadith/apps.py
+++ hadith/apps.py
@@ -1,10 +1,12 @@
 import json
+import os
 from django.apps import AppConfig
+from telegram_bot.credentials import BASE_DIR
 
 
 class HadithConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "hadith"
-    file_path = "fixtures/hadith.json"
+    file_path = os.path.join(BASE_DIR, "fixtures/hadith.json")
     with open(file_path) as hadith_file:
         hadiths = json.load(hadith_file)
