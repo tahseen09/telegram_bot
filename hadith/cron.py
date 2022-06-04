e92ea0757a0b97e2f285fd9d1224ffe271a88377
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2022-06-05 03:13:14 +0530

    Remove Cron

--- hadith/cron.py
+++ hadith/cron.py
@@ -1,5 +1,7 @@
-from hadith.services import send_hadith_to_all_users
-
-
 def send_daily_hadith():
+    from hadith.services import send_hadith_to_all_users
     send_hadith_to_all_users()
+
+
+if __name__ == "__main__":
+    send_daily_hadith()
--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_crontab',
     'hadith'
 ]
 
@@ -89,11 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-]
-
-
-CRONJOBS = [
-    ('* * * * *', 'hadith.cron.send_daily_hadith')
 ]
 
 # Internationalization
