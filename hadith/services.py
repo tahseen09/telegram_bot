df4c7950d6b9b37bc85917e2abfa7990a568e577
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2022-06-05 01:59:35 +0530

    Refactor

--- hadith/services.py
+++ hadith/services.py
@@ -1,14 +1,40 @@
 import random
+import telegram
+
 from hadith.apps import HadithConfig
 from hadith.models import Chat
+from telegram_bot.credentials import BOT_TOKEN, APP_URL
 
 
-def get_random_hadith():
+bot = telegram.Bot(token=BOT_TOKEN)
+
+
+def get_random_hadith() -> str:
     hadiths = HadithConfig.hadiths
     hadith = random.choice(hadiths)
     return f"{hadith['En_Sanad']} \n {hadith['En_Text']}"
 
 
-def create_chat(chat_id: str):
+def create_chat(chat_id: str) -> None:
     chat = Chat(chat_id=chat_id)
     chat.save()
+
+def get_response(text: str) -> str:
+    text_response_map = {
+        "/start": "Assalamu-aliakum! I am Tahseen Rahman.\nUse this bot to read Hadiths and make your day better. You can message anything and you shall receive a hadith in response. Or you can also message /subscribe to receive Hadiths Daily. Jazakallah.",
+        "/subscribe": "Jazakallah. You have been subscribed to daily hadiths. To read a hadith right now, message anything."
+    }
+    return text_response_map.get(text, get_random_hadith())
+
+def send_message(chat_id: str, message: str) -> bool:
+    try:
+        bot.sendMessage(chat_id=chat_id, text=message)
+        return True
+    except Exception:
+        return False
+
+def get_telegram_update_object(request_body: dict) -> telegram.Update:
+    return telegram.Update.de_json(request_body, bot)
+
+def set_telegram_webhook():
+    return bot.setWebhook(f"{APP_URL}{BOT_TOKEN}")
--- hadith/views.py
+++ hadith/views.py
@@ -1,35 +1,28 @@
 import json
-import telegram
 
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-from hadith.services import create_chat, get_random_hadith
-
-from telegram_bot.credentials import BOT_TOKEN, APP_URL
-
-bot = telegram.Bot(token=BOT_TOKEN)
+from hadith.services import create_chat, get_response, get_telegram_update_object, send_message, set_telegram_webhook
 
 
 @csrf_exempt
 def respond(request):
     request_body = json.loads(request.body)
-    update = telegram.Update.de_json(request_body, bot)
+    update = get_telegram_update_object(request_body)
     chat_id = update.message.chat.id
     # msg_id = update.message.message_id
     text = update.message.text.encode('utf-8').decode()
-    if text == "/start":
-        response = "Assalamu-aliakum! I am Tahseen Rahman.\nUse this bot to read Hadiths and make your day better. You can message anything and you shall receive a hadith in response. Or you can also message /subscribe to receive Hadiths Daily. Jazakallah."
-    elif text == "/subscribe":
+    response = get_response(text)
+
+    if text == "/subscribe":
         create_chat(chat_id)
-        response = "Jazakallah. You have been subscribed to daily hadiths. To read a hadith right now, message anything."
-    else:
-        response = get_random_hadith()
-    bot.sendMessage(chat_id=chat_id, text=response)
+
+    send_message(chat_id, response)
     return HttpResponse()
 
 
 def set_webhook(request):
-    webhook = bot.setWebhook(f"{APP_URL}{BOT_TOKEN}")
+    webhook = set_telegram_webhook()
     if webhook:
         return HttpResponse("Webhook Set")
 
