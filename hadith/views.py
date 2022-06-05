2233dc9746eaf8e4829dd14d0292f00bfe174abf
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2022-06-05 19:50:36 +0530

    Handle where chat_id is not present

--- hadith/views.py
+++ hadith/views.py
@@ -3,21 +3,21 @@ import json
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from hadith.services import (
-    create_chat,
     get_response,
     get_telegram_update_object,
     send_message,
     set_telegram_webhook,
-    update_language_preference,
 )
-from hadith.constants import LANGUAGE_COMMAND, SUBSCRIBE_COMMAND, SUPPORTED_LANGUAGES
 
 
 @csrf_exempt
 def respond(request):
     request_body = json.loads(request.body)
     update = get_telegram_update_object(request_body)
-    chat_id = update.message.chat.id
+    try:
+        chat_id = update.message.chat.id
+    except AttributeError:
+        return HttpResponseBadRequest()
     # msg_id = update.message.message_id
     text = update.message.text.encode("utf-8").decode()
     responses = get_response(text, chat_id)
